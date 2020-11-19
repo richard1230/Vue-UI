1cc22fd6f9664acda85c03d95ea9a5fec683880b
richard1230 <yongjinhu1230@gmail.com>
2020-11-19 16:44:03 +0800

    initial project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hi</div>
+  <richard/>
+</template>
+
+<script>
+import richard from './components/richard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // 左边是标签名,右边是组件的引用
+    // HelloWorld:HelloWorld===>其实课缩写为一个HelloWorld
+    richard
+  }
+}
+</script>
---
+++ src/components/richard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    hello world,vue,这是我的第一个组件
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "richard.vue"
+  }
+</script>
+
+<style scoped>
+
+</style>
