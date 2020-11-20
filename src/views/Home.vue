ec7ee2fc9b6fb9fe885755e10a144931856f27d6
richard1230 <yongjinhu1230@gmail.com>
2020-11-20 16:25:18 +0800

    add link in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-     <Topnav />
-    <div>
-      <div class="banner">
-        <h1></h1>gulu-UI</h1>
-        <h2>an awesome UI</h2>
-      </div>
+    <Topnav />
+    <div class="banner">
+      <h1>GuLu-UI</h1>
+      <h2>an-awesomeful-UI </h2>
       <p class="actions">
-        <a>github</a>
-        <a>start</a>
+        <a href="https://www.github.com">GitHub</a>
+        <router-link to="/doc">Start</router-link>
       </p>
     </div>
   </div>
 </template>
 <script lang="ts">
   import Topnav from "../components/Topnav.vue";
-  export  default {
+  export default {
     components: { Topnav },
   };
 </script>
@@ -25,7 +23,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    //https://www.ruanyifeng.com/blog/2015/07/flex-examples.html
     flex-direction: column;
     background: lightgreen;
     > .actions{
@@ -33,7 +30,6 @@
       a {
         margin:0 8px;
         background: #fff;
-        /*内联元素制定宽高没有效果. 使用这个字段display: inline-block;*/
         display: inline-block;
         $h: 28px;
         height: $h;
