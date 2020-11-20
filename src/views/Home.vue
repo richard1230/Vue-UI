7159ff278d54e78c65395eead834ef3b3c5745b3
richard1230 <yongjinhu1230@gmail.com>
2020-11-20 16:04:33 +0800

    home 页面的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-     <Topnav/>
+     <Topnav />
     <div>
       <div class="banner">
         <h1></h1>gulu-UI</h1>
@@ -14,11 +14,33 @@
   </div>
 </template>
 <script lang="ts">
-  import Topnav from '../components/Topnav.vue';
+  import Topnav from "../components/Topnav.vue";
   export  default {
-    components: {Topnav}
-  }
+    components: { Topnav },
+  };
 </script>
 <style lang="scss" scoped>
-
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    //https://www.ruanyifeng.com/blog/2015/07/flex-examples.html
+    flex-direction: column;
+    background: lightgreen;
+    > .actions{
+      padding: 8px 0;
+      a {
+        margin:0 8px;
+        background: #fff;
+        /*内联元素制定宽高没有效果. 使用这个字段display: inline-block;*/
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
 </style>
