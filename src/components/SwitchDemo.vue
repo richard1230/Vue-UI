e42ee94ce9aff238919fd164546d86dcf6cc7e99
richard1230 <yongjinhu1230@gmail.com>
2020-11-21 16:35:53 +0800

    可以接受value和input事件 1.Switch.vue里面: props;setup(props,context);class="{checked:value}" 2.SwitchDemo里面：<Switch :value="y" @input="y = $event"/>

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
-<Switch/>
+<!--    冒号表示传的是变量y,没有冒号表示传的是字符串y-->
+<Switch :value="y" @input="y = $event"/>
   </div>
 </template>
 
 <script lang="ts">
   import Switch from '../lib/Switch.vue'
+  import { ref } from 'vue'
   export default {
-    components:{Switch}
+    components:{Switch},
+    setup(){
+      const y = ref(false)
+      return {y}
+    }
   }
 </script>
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,30 +1,34 @@
 <template>
-  <button @click="toggle" :class="{checked}">
-    <span>
-
-  </span>
-  </button>
+  <button @click="toggle" :class="{checked:value}">
+    <span></span></button>
+<!--  <div>{{value}}</div>-->
 </template>
 <script lang="ts">
-  import {ref} from "vue"
-  export default {
-    setup(){
-    const checked = ref(false)
-      const toggle = ()=>{
-        checked.value = !checked.value
-      }
-      //这里必须要导出toggle,不然模板里面不能用
-      return {checked,toggle}
+  import {ref} from 'vue';
 
+  export default {
+    props: {
+      value: Boolean
+    },
+    setup(props,context) {
+      // const checked = ref(false);
+      const toggle = () => {
+        //将当前的value取反，通过input发给外面
+        //!props.value与SwitchDemo里面的$event对应
+        //input和SwitchDemo里面的input对应
+        context.emit('input',!props.value)
+      };
+      //这里必须要导出toggle,不然模板里面不能用
+      return { toggle};
     }
-  }
+  };
 
 </script>
 
 <style lang="scss" scoped>
- $h: 22px;
+  $h: 22px;
   $h2: $h - 4px;
-  button{
+  button {
     height: $h;
     width: $h*2;
     border: none;
@@ -32,27 +36,28 @@
     border-radius: $h/2;
     position: relative;
   }
-  span{
+
+  span {
     position: absolute;
     top: 2px;
     left: 2px;
     height: $h2;
     width: $h2;
-    background:white;
+    background: white;
     border-radius: $h2 / 2;
     /*如果left有变化，就让它变化1秒钟*/
-    transition: left 1s;
+    transition: left 250ms;
   }
 
- button.checked{
-   background: blue;
- }
-
-  button.checked > span{
-    left:calc(100% - #{$h2} - 2px) ;
+  button.checked {
+    background: blue;
   }
 
-  button:focus{
+  button.checked > span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+
+  button:focus {
     outline: none;
   }
 
