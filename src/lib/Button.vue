f928ad2aa4547af4ef7013284ad30390ca934b81
richard1230 <yongjinhu1230@gmail.com>
2020-11-22 14:11:17 +0800

    基础样式

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,22 +1,64 @@
 <template>
-    <button class="gulu-button" :class="`theme-${theme}`">
-      <slot/>
-    </button>
+  <button class="gulu-button" :class="{[`theme-${theme}`]:theme}">
+    <slot/>
+  </button>
 </template>
 <script lang="ts">
 
   export default {
-    props:{
-       theme:{
-         type: String,
-         default:'button'
-       }
+    props: {
+      theme: {
+        type: String,
+        default: 'button'
+      },
 
     },
-  }
+  };
 </script>
 <style lang="scss" scoped>
-  div{
-    border: 1px solid red;
+  $h: 32px;
+  $border-color: #d9d9d9;
+  $color: #333;
+  $blue: #40a9ff;
+  $radius: 4px;
+  .gulu-button {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    //光标呈现为指示链接的指针（一只手）
+    cursor: pointer;
+   //https://codepen.io/clairecodes/pen/POjKXY
+    //行内布局
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    //http://www.ruanyifeng.com/blog/2018/07/white-space.html
+    //white-space属性为nowrap时，不会因为超出容器宽度而发生换行。
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    //https://developer.mozilla.org/zh-CN/docs/Web/CSS/box-shadow
+    //使用box-shadow来添加阴影效果
+    box-shadow: 0 1px 0 fade-out(black, 0.95);
+
+    & + & {
+      margin-left: 8px;
+    }
+
+    &:hover,
+    &:focus {
+      color: $blue;
+      border-color: $blue;
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    &::-moz-focus-inner {
+      border: 0;
+    }
   }
 </style>
