293571cd31af4809b21004a38c1e806f71ba4db6
richard1230 <yongjinhu1230@gmail.com>
2020-11-23 22:14:34 +0800

    如何在运行时确认子组件类型

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,14 +2,16 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tab title="导航1">内容1</Tab>
-  <Tab title="导航2">内容2</Tab>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
+
 
 </template>
 <script lang="ts">
-  import Tab from '../lib/Tab.vue'
   import Tabs from '../lib/Tabs.vue'
-
+  import Tab from '../lib/Tab.vue'
   export default {
     components:{Tab,Tabs}
   }
