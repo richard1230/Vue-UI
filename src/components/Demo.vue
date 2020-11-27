84e1402c631fbf17fc8b5a22633df66db26715a4
richard1230 <yongjinhu1230@gmail.com>
2020-11-27 22:16:16 +0800

    显示 demo 标题

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,34 +1,29 @@
 <template>
-  <div>
-    <h1>Switch 组件示例 </h1>
-    <Demo :component="Switch1Demo" />
-    <Demo :component="Switch2Demo" />
+  <div class="demo">
+    <h2>{{component.__sourceCodeTitle}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button>查看代码</Button>
+    </div>
+    <div class="demo-code">
+      <pre class="language-html" v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')" />
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
-  import Button from '../lib/Button.vue'
-  import Switch1Demo from './Switch1.demo.vue'
-  import Switch2Demo from './Switch2.demo.vue'
   import 'prismjs';
   import 'prismjs/themes/prism.css'
-  import Demo from './Demo.vue'
   const Prism = (window as any).Prism
-
-  import {
-    ref
-  } from 'vue'
   export default {
-    components: {
-      Button,
-      Demo
+    props: {
+      component: Object
     },
     setup() {
-      const bool = ref(false)
       return {
-        bool,
-        Switch1Demo,
-        Switch2Demo,
         Prism
       }
     }
@@ -37,30 +32,24 @@
 
 <style lang="scss" scoped>
   $border-color: #d9d9d9;
-
   .demo {
     border: 1px solid $border-color;
     margin: 16px 0 32px;
-
     >h2 {
       font-size: 20px;
       padding: 8px 16px;
       border-bottom: 1px solid $border-color;
     }
-
     &-component {
       padding: 16px;
     }
-
     &-actions {
       padding: 8px 16px;
       border-top: 1px dashed $border-color;
     }
-
     &-code {
       padding: 8px 16px;
       border-top: 1px dashed $border-color;
-
       >pre {
         line-height: 1.1;
         font-family: Consolas, 'Courier New', Courier, monospace;
