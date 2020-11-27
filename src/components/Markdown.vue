bd3922c752349155ae3007b03de7487e90614bc8
richard1230 <yongjinhu1230@gmail.com>
2020-11-27 17:12:43 +0800

    代码优化1

---
+++ src/components/Markdown.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="markdown-body" v-html="content">
+  </article>
+</template>
+
+<script lang="ts">
+  import {ref} from 'vue'
+  export default {
+    props:{
+      path:{
+        type: String,
+        required: true
+      }
+    },
+    setup(props){
+      const content = ref<string>(null)
+      //异步加载,动态引入，通过then异步获取值
+      import(props.path).then(result=>{
+        content.value = result.default
+      })
+
+      return {content}
+    },
+
+  }
+</script>
