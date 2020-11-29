7d3fa358fd59436c83bb3c2adbf8054bf05f8283
richard1230 <yongjinhu1230@gmail.com>
2020-11-29 12:14:57 +0800

    改造 Dialog demo

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,65 +1,22 @@
 <template>
   <div>Dialog 示例</div>
-  <h1>示例1</h1>
-  <Button @click="toggle">toggle</Button>
-<!--  默认设置点击遮罩层没有响应-->
-<!--  用户点击ok/cancle 的时候先执行 f1/f2，而后在关闭，这里是一个非常重要的需求
- 因为在点击OK之前可能还需要进行其他操作，不能立即关闭，所以这里需要添加一个函数f1-->
-  <Dialog v-model:visible="x" :closeOnClickOverlay = "false" :ok="f1" :cancel="f2">
-    <template v-slot:content>
-      <div>hi</div>
-      <div>hi2</div>
-    </template>
-    <template v-slot:title>
-      <strong>加粗的标签</strong>
-    </template>
-  </Dialog>
-  <h1>示例2</h1>
-    <Button @click="showDialog">show</Button>
-</template>
+  <Demo :component="Dialog1Demo" />
+  <Demo :component="Dialog2Demo" />
+  </template>
 <script lang="ts">
-  import Dialog from '../lib/Dialog.vue'
-  import Button from '../lib/Button.vue';
-  import {ref} from "vue";
-  import {openDialog} from '../lib/openDialog';
 
+  import Demo from './Demo.vue';
+  import Dialog1Demo from './Dialog1.demo.vue';
+  import Dialog2Demo from './Dialog2.demo.vue';
   export default {
-    components:{
-      Button,
-      Dialog
+    components: {
+      Demo
     },
-
-    setup(){
-      const x = ref(false);
-      const toggle = ()=>{
-        x.value = !x.value
-      }
-
-      const f1=()=>{
-        console.log(1)
-        return false
-      }
-      const f2=()=> {
-        console.log(2)
-      }
-
-      const showDialog = ()=>{
-        openDialog({
-          title:'标题',
-          content:'你好',
-          ok(){
-            console.log('ok')
-            return false
-          },
-          cancel(){
-            console.log('cancel')
-          }
-        })
-      }
+    setup() {
       return {
-        x,toggle,f1,f2,showDialog
+        Dialog1Demo,
+        Dialog2Demo
       }
     }
   }
-
 </script>
