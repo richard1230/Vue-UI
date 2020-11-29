72581136a9122eebfabc6d6841d0f97cd1477968
richard1230 <yongjinhu1230@gmail.com>
2020-11-29 13:43:02 +0800

    last官网优化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,31 +12,31 @@
     </div>
   </div>
 
-<div class="features">
-  <ul>
-    <li>
-      <svg>
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <h3>基于 Vue 3</h3>
-      <p>骄傲地使用了 Vue 3 Composition API</p>
-    </li>
-    <li>
-      <svg>
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <h3>基于 TypeScript </h3>
-      <p>源代码采用 TypeScript 书写</p>
-    </li>
-    <li>
-      <svg>
-        <use xlink:href="#icon-light"></use>
-      </svg>
-      <h3>代码易读</h3>
-      <p>每个组件的源代码都极其简洁</p>
-    </li>
-  </ul>
-</div>
+    <div class="features">
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>骄傲地使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript </h3>
+          <p>源代码采用 TypeScript 书写</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -62,6 +62,7 @@
 
     @media (min-width: 800px) {
       width: 800px;
+
       >ul {
         >li {
           width: 50%;
@@ -71,9 +72,10 @@
 
     @media (min-width: 1200px) {
       width: 1200px;
+
       >ul {
         >li {
-          width: 33.3333%;
+          width: 33.333333333%;
         }
       }
     }
@@ -83,7 +85,6 @@
       flex-wrap: wrap;
 
       >li {
-        width: 400px;
         margin: 16px 0;
         display: grid;
         justify-content: start;
