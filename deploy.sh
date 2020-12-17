94c21c5f32b34ad4bdf89889f6eda7894d64ecf9
richard1230 <yongjinhu1230@gmail.com>
2020-12-17 15:35:35 +0800

    add readme.md

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:richard1230/Vue-UI-website.git &&
+#git remote add origin git@github.com:richard1230/Vue-UI-website.git &&
+git remote add origin git@gitee.com:richard1230/gulu-vue-ui.git &&
+
 git push -f -u origin master &&
 cd ..
-echo https://richard1230.github.io/Vue-UI-website/index.html#/
+#echo https://richard1230.github.io/Vue-UI-website/index.html#/
--- src/markdown/install.md
+++ src/markdown/install.md
@@ -1,4 +1,24 @@
-# 安装
+## 项目环境
+
+```
+$node -v
+v12.13.1
+$yarn -v
+1.22.4
+$npm -v
+6.12.1
+
+注意在使用包管理工具的时候，两个不能混合使用。推荐yarn 
+
+//yarn 设置淘宝源
+yarn config set registry https://registry.npm.taobao.org
+
+//node-sass设置镜像
+yarn config set sass_binary_site http://cdn.npm.taobao.org/dist/node-sass -g
+
+```
+
+# 快速上手
 
 打开终端运行下列命令：
 
